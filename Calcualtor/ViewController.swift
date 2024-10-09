c5867baae3c06e1e30c1c6f21a8d584b582f3b3a
sandro <beqa@beqas-MacBook-Pro.local>
2024-10-09 11:26:22 -0700

    layout

---
+++ Calcualtor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calcualtor
+//
+//  Created by beqa on 09.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
