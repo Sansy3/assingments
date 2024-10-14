db9ef3e98db335d03a11a8bf208d76f9d00150d0
sandro <beqa@beqas-MacBook-Pro.local>
2024-10-14 05:56:04 -0700

    assingment14

---
+++ CalculatorLayoutCode/CalculatorLayoutCode/CalculatorVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorLayoutCode
+//
+//  Created by beqa on 13.10.24.
+//
+
+import UIKit
+
+class CalculatorVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .purple
+    }
+
+
+}
+
---
+++ CalculatorLayoutCode/CalculatorLayoutCode/SettingsVC.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsVC.swift
+//  CalculatorLayoutCode
+//
+//  Created by beqa on 13.10.24.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
