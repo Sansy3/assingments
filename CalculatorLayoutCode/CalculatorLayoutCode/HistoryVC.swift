25a40e7a377a9675ba1778c6fcd044df28845792
sandro <beqa@beqas-MacBook-Pro.local>
2024-10-16 09:17:02 -0700

    Averagecalc

---
+++ CalculatorLayoutCode/CalculatorLayoutCode/HistoryVC.swift
@@ -0,0 +1,29 @@
+//
+//  HistoryVC.swift
+//  CalculatorLayoutCode
+//
+//  Created by beqa on 16.10.24.
+//
+
+import UIKit
+
+class HistoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
