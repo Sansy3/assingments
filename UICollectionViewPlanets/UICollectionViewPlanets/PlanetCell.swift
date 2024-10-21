6521c2902ee56f128e556f81fdf827d37e635954
sandro <beqa@beqas-MacBook-Pro.local>
2024-10-21 04:50:10 -0700

    UICollectionViewPlanets

---
+++ UICollectionViewPlanets/UICollectionViewPlanets/PlanetCell.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetCell.swift
+//  UICollectionViewPlanets
+//
+//  Created by beqa on 20.10.24.
+//
+
+import UIKit
+
+class PlanetCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
