7529a9a4d9fb9a570716ea179a794ef7fd4ccdbc
sandro <beqa@beqas-MacBook-Pro.local>
2024-11-11 05:27:22 -0800

    Modularization

---
+++ Assingment19/Assingment19Tests/Assingment19Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Assingment19Tests.swift
+//  Assingment19Tests
+//
+//  Created by beqa on 30.10.24.
+//
+
+import Testing
+@testable import Assingment19
+
+struct Assingment19Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
