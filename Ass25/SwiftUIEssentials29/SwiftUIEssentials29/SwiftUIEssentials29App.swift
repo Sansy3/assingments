ebbbb1d98d2233246452f954c96117f182f96543
Sandro <sandro.shedania.1@iliauni.edu.ge>
2024-12-09 05:22:47 -0800

    SwifteEssentialsASS29

---
+++ Ass25/SwiftUIEssentials29/SwiftUIEssentials29/SwiftUIEssentials29App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIEssentials29App.swift
+//  SwiftUIEssentials29
+//
+//  Created by beqa on 09.12.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIEssentials29App: App {
+    var body: some Scene {
+        WindowGroup {
+            ProfileView()
+        }
+    }
+}
